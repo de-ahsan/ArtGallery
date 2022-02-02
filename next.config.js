8992ff8d6b1817a4e6d486bf92a96e8c9327fd4c
Ahsan Elahi <mail.ahsan.ellahi@gmail.com>
2022-02-02 11:55:29 -0800

    Added eslint with basic rules

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['artic-web.imgix.net'],
-  }
+  },
+  eslint: {
+    dirs: ['pages', 'styles']
+  },
 }
 
 module.exports = nextConfig
